63c25bafbe6ce1e6ec1b83fe27a36a2d5e094708
Hunter <davhunter@deloitte.ca>
2021-09-28 20:12:08 -0400

    completed module 3

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,16 +2,31 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 import NewTodoForm from './NewTodoForm';
+import { connect } from 'react-redux';
+import { removeTodo, markTodoAsCompleted } from './actions';
 
-const TodoList = ({ todos = [{ text: 'Hello' }] }) => {
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => {
   return (
     <div className="list-wrapper">
       <NewTodoForm />
       {todos.map((todo) => (
-        <TodoListItem todo={todo} />
+        <TodoListItem
+          todo={todo}
+          onRemovePressed={onRemovePressed}
+          onCompletedPressed={onCompletedPressed}
+        />
       ))}
     </div>
   );
 };
 
-export default TodoList;
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({ todo }) => {
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => {
   return (
     <div className="todo-item-container">
       <h3>{todo.text}</h3>
       <div className="buttons-container">
-        <button className="completed-button">Mark as Completed</button>
-        <button className="remove-button">Remove</button>
+        {todo.isCompleted ? null : (
+          <button
+            className="completed-button"
+            onClick={() => onCompletedPressed(todo.text)}
+          >
+            Mark as Completed
+          </button>
+        )}
+        <button
+          className="remove-button"
+          onClick={() => onRemovePressed(todo.text)}
+        >
+          Remove
+        </button>
       </div>
     </div>
   );
